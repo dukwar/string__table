a2b7898c21aa6e389de38feb579cafa104919b1e
dukwar <d.clopow@yandex.ru>
2021-07-25 05:03:45 +0300

    word count function changed

--- src/hooks/useCountLine.hook.ts
+++ src/hooks/useCountLine.hook.ts
@@ -5,19 +5,15 @@ import deburr from "lodash/deburr";
 // custom hook useCountLine
 export const useCountLine = () => {
 
-    // 1. the function uses the deburr function from the lodash library, which strips off unnecessary diacritics symbols at the line
-    // 2. does not work for the Russian alphabet, so you should use the normalize method
-    // 3. normalize returns the Unicode normalization form, but adds unnecessary symbols to the string that the replace range does not see,
-    // so you should strip the string and leave everything you need, additionally removing the dash with spaces
     const getCountWord = useCallback((str: string) => {
-        const newStr = deburr(str)
-        return newStr.normalize('NFD').replace(/\s-\s/gi, ' ').replace(/[^-,/a-zа-яёі\s\d+−–—]/gi, '').match(/[-a-zа-яёі\d+]+/gi)?.length
+        console.log(str)
+        let regexp = /[\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+/gui
+        return str.match(regexp)?.length
 
     }, [])
 
-    // 1. in this case, we also use the deburr function, the normalize method and search for a given range
+    // 1. in this case, we use the deburr function from lodash library, the normalize method and search for a given range
     // 2. remove the "й" since the normalize method cuts off all symbols and the letter turns into the letter "и"
-    // 3. Thus, the delimiter can be anything
     const getCountVowels = useCallback((str: string) => {
         const newStr = str.replace(/й/gi, '')
         const newStrUnDiacritics = deburr(newStr)
--- src/hooks/useValidate.hook.ts
+++ src/hooks/useValidate.hook.ts
@@ -4,24 +4,23 @@ import {useCallback} from "react";
 
 export const useValidate = () => {
 
-    // 1. in this function, we cut off all leading zeros and check if the numbers entered by the user go beyond the set
+    // 1. in this function, we remove all spaces, and also separate elements with a separator - ";" or "," and check if the numbers entered by the user go beyond the set
     const isValidNums = useCallback((str: string) => {
 
-        const newStr = str.replace(/^0+/g, '').match(/\d+/gi)
-        return newStr && newStr.map((item) => Number(item)).some((item) => item < 1 || item > 20)
+        const newStr = str.replace(/\s/g, '').split(/,|;/)
+        const index = newStr.indexOf('')
+        index > -1 && newStr.splice(index,1)
+
+        return newStr && newStr.map((item) => Number(item)).some((item) => item < 1 || item > 20 || isNaN(item))
 
     }, [])
 
-    // 1. in this case, we also strip off all leading zeros,
-    // we look for all numerical values, we search for unique values, and also leave only valid values
-    // 2. However, formik does not allow submitting the form if errors occurred during validation, so the last part of the code remains frozen
-    // 3. Also, one more task condition has not been met - the numbers in the input must be separated by "," or ";", which in principle loses the need when using the "replace-match" methods
-    // 4. any letters and other values ignored as they are not iterable
+    // 1. in this case, we remove all spaces, and also separate elements with a separator - ";" or ",", search for unique values, and also leave only valid values
 
     const getValidHttpArray = useCallback((str: string) => {
 
-        const newStr = str.replace(/^0+/g, '').match(/\d+/gi)
-        return newStr && Array.from(new Set(newStr)).map((item) => Number(item)).filter((item) => item > 0 && item <= 20)
+        const newStr = str.replace(/\s/, '').split(/,|;/)
+        return newStr && Array.from(new Set(newStr)).map((item) => Number(item))
 
     }, [])
 
